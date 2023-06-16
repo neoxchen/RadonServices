7de6f65aff0e7f1bf2e1ddba5ed9c8e2cc562339
Breeze <101343310+neoxchen@users.noreply.github.com>
2023-06-15 21:46:52 -0700

    Added pipeline shutdown listener

--- backend/endpoints/pipeline_status.py
+++ backend/endpoints/pipeline_status.py
@@ -2,7 +2,7 @@ from flask import make_response, request
 from flask_restful import Resource
 
 from endpoints.wrappers import safe_request
-from shared_states import CONTAINER_STATUS
+from shared_states import CONTAINER_STATUS, get_pipeline_type, PIPELINE_CONTAINERS
 
 
 class PipelineStatusEndpoint(Resource):
@@ -38,5 +38,24 @@ class PipelineStatusEndpoint(Resource):
             "successes": body["successes"],
             "fails": body["fails"]
         }
+
+        return make_response({"message": "OK"}, 200)
+
+    @safe_request
+    def delete(self, container_id, uid):
+        """ Deletes the status of the current pipeline """
+        # Remove from pipeline dict
+        pipeline_type = get_pipeline_type(container_id)
+        if pipeline_type is not None:
+            to_remove = None
+            for container in PIPELINE_CONTAINERS[pipeline_type]:
+                if container.container_id == container_id:
+                    to_remove = container
+                    break
+            if to_remove is not None:
+                PIPELINE_CONTAINERS[pipeline_type].remove(to_remove)
+
+        # Remove from status dict
+        del CONTAINER_STATUS[container_id]
 
         return make_response({"message": "OK"}, 200)
